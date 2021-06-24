bbe1fe9c426b87f206731a5096b98cb6744451a2
james <jcotab@gmail.com>
2021-06-23 22:41:39 -0700

    format

--- src/main/java/com/technews/model/Post/Post.java
+++ src/main/java/com/technews/model/Post/Post.java
@@ -14,147 +14,147 @@ import java.util.Objects;
 @Table(name = "post")
 public class Post  implements Serializable {
 
-@Id
-@GeneratedValue(strategy = GenerationType.AUTO)
-private Integer id;
-private String title;
-private String postUrl;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	private String title;
+	private String postUrl;
 
-@Transient
-private String userName;
 
-@Transient
-private int voteCount;
-private Integer userId;
+	@Transient
+	private String userName;
 
-@NotNull
-@Temoral(TemporalType.DATE)
-@Column(name = "posted_at")
-private Date postedAt = new Date();
+	@Transient
+	private int voteCount;
+	private Integer userId;
 
-@NotNull
-@Temporal(TemporalType.DATE)
-@Column(name = "updated_at")
-private Date updatedAt = new Date();
+	@NotNull
+	@Temoral(TemporalType.DATE)
+	@Column(name = "posted_at")
+	private Date postedAt = new Date();
 
-public Post() {
+	@NotNull
+	@Temporal(TemporalType.DATE)
+	@Column(name = "updated_at")
+	private Date updatedAt = new Date();
 
-}
+	public Post() {
 
-public Post(Integer Id, String title, String postUrl, int voteCount, Integer userId) {
-	this.id = id;
-	this.title = title;
-	this.postUrl = postUrl;
-	this.voteCount = voteCount;
-	this.userId = userId;
-}
-
-public Integer getId() {
-	return id;
-}
-
-public void setId(Integer Id) {
-	this.id = id;
-}
-
-public String getTitle() {
-	return title; 
-}
-
-public void setTitle(String title) {
-	this.title = title;
-}
-
-public String getPostUrl() {
-	return postUrl;
-}
-
-public void setPostUrl(String postUrl) {
-	this.postUrl = postUrl;
-}
-
-public String getUserName() {
-	return userName;
-}
-
-public void setUserName(String userName) {
-	this.userName = userName;
-}
-
-public int getVoteCount() {
-	return voteCount;
-}
-
-public void setVoteCount(int voteCount) {
-	this.voteCount = voteCount;
-}
-
-public Integer getUserId() {
-	return userId;
-}
-
-public void setUserId(Integer userId) {
-	this.userId = userId;
-}
-
-public Date getPostedAt() {
-	return postedAt;
-}
-
-public void setPostedAt(Date postedAt) {
-	this.postedAt = postedAt;
-}
-
-public Date getUpdatedAt() {
-	return updatedAt;
-}
-
-public void setUpdatedAt(Date updatedAt) {
-	this.updatedAt = updatedAt;	
-}
-
-public List<Comment> getComments() {
-	return comments;
-}
-
-public void getComments(List<Comment> comments) {
-	this.comments = comments;
-}
-
-@Override
-public boolean equals(Object o) {
-	if(this == o ) return true;
-	if(!(o instanceof Post)) return false;
-	Post post (Post) o;
-	return getVoteCount() == post.getVoteCount() &&
-		Objects.equals(getId(), post.getId()) &&
-		Objects.equals(getTitle(), post.getTitle()) &&
-		Objects.equals(getPostUrl(), post.getPostUrl()) &&
-		Objects.equals(getUserName(), post.getUserName()) &&
-		Objects.equals(getUserId(), post.getUserId()) &&
-		Objects.equals(getPostedAt(), postgetPostedAt()) &&
-		Objects.equals(getUpdatedAt(), post.getUpdatedAt()) &&
-		Objects.equals(getComments(), post.getComments()) &&
-}
-
-@Override
-public int hashCode() {
-	return bjects.hash(getId(), getTitle(), getPostUrl(), getUserName(), getVoteCount(), getUserId(), getPostedAt(), getUpdatedAt(), getComments());
-}
-
-@Override
-public String toString() {
-	return "Post{" +
-		"id=" + id +
-		", title='" + title + '\'' +
-		", postUrl='" + postUrl + '\'' +
-		", userName='" + userName + '\'' +
-		", voteCount=" + voteCount +
-		", userId=" + userId +
-		", postedAt=" + postedAt +
-		", updatedAt=" + updatedAt +
-		", comments=" + comments +
-		'}';
 	}
 
+	public Post(Integer Id, String title, String postUrl, int voteCount, Integer userId) {
+		this.id = id;
+		this.title = title;
+		this.postUrl = postUrl;
+		this.voteCount = voteCount;
+		this.userId = userId;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer Id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title; 
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPostUrl() {
+		return postUrl;
+	}
+
+	public void setPostUrl(String postUrl) {
+		this.postUrl = postUrl;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public int getVoteCount() {
+		return voteCount;
+	}
+
+	public void setVoteCount(int voteCount) {
+		this.voteCount = voteCount;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public Date getPostedAt() {
+		return postedAt;
+	}
+
+	public void setPostedAt(Date postedAt) {
+		this.postedAt = postedAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;	
+	}
+
+	public List<Comment> getComments() {
+		return comments;
+	}
+
+	public void getComments(List<Comment> comments) {
+		this.comments = comments;
+	}	
+
+	@Override
+	public boolean equals(Object o) {
+		if(this == o ) return true;
+		if(!(o instanceof Post)) return false;
+		Post post (Post) o;
+		return getVoteCount() == post.getVoteCount() &&
+			Objects.equals(getId(), post.getId()) &&
+			Objects.equals(getTitle(), post.getTitle()) &&
+			Objects.equals(getPostUrl(), post.getPostUrl()) &&
+			Objects.equals(getUserName(), post.getUserName()) &&
+			Objects.equals(getUserId(), post.getUserId()) &&
+			Objects.equals(getPostedAt(), postgetPostedAt()) &&
+			Objects.equals(getUpdatedAt(), post.getUpdatedAt()) &&
+			Objects.equals(getComments(), post.getComments()) &&
+	}
+
+	@Override
+	public int hashCode() {
+		return bjects.hash(getId(), getTitle(), getPostUrl(), getUserName(), getVoteCount(), getUserId(), getPostedAt(), getUpdatedAt(), getComments());
+	}
+
+	@Override
+	public String toString() {
+		return "Post{" +
+			"id=" + id +
+			", title='" + title + '\'' +
+			", postUrl='" + postUrl + '\'' +
+			", userName='" + userName + '\'' +
+			", voteCount=" + voteCount +
+			", userId=" + userId +
+			", postedAt=" + postedAt +
+			", updatedAt=" + updatedAt +
+			", comments=" + comments +
+			'}';
+		}
 }
